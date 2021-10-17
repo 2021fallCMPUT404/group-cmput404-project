b5b35efd404cb56dc66588f49204823d4c5ad9cf
ShafinRez <shafinrezwan123@gmail.com>
2021-10-17 11:10:17 -0600

    posts

--- socialNetwork/socialNetwork/wsgi.py
+++ socialNetwork/socialNetwork/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for socialdistribution project.
+WSGI config for socialNetwork project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialdistribution.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialNetwork.settings')
 
 application = get_wsgi_application()

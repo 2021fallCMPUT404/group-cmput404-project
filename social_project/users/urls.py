8e54aa4b5bb72ef53f5a17c06b88212afea27aa7
JohnChen97 <56984799+JohnChen97@users.noreply.github.com>
2021-10-23 11:51:12 -0600

    Delete social_project directory

--- social_project/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from django.urls import path
-from users import views
-
-urlpatterns = [path('', views.index, name='index')]

3a1305d36dfa4e0d6c198b881a2b3d2ed1047e39
JohnChen97 <chenjionghao@yahoo.com>
2021-10-24 21:04:48 -0600

    merge jionghao and Gus

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -2,28 +2,46 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.http import HttpResponse
 import uuid
-from django import forms	
+from django import forms
 from django.forms.widgets import Textarea
+import datetime
+
+
+def user_directory_path(instance, filename):
+
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    #return 'user_{0}/{1}'.format(instance.user.id, filename)
+    return 'images/users/user_{0}/{1}'.format(instance.user.id, filename)
+
+
 # Create your models here.
+
 
 #TODO: MERGE USER_PROFILE INTO USER
 class User(AbstractUser):
-	pass
+    pass
+
 
 class Create_user(forms.Form):
-	username = forms.CharField(initial='')
-	password = forms.CharField(widget=forms.PasswordInput())
-	confirm_password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(initial='')
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
 
 class User_Profile(models.Model):
-	type = "author"
-	#id = uuid.uuid4()
-	host = None
-	displayName = models.CharField(max_length=60)
-	profileImage = None
-	github = None
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = "author"
+    #id = uuid.uuid4()
+    host = None
+    displayName = models.CharField(max_length=60)
+    profileImage = models.ImageField(upload_to=user_directory_path)
+    github = models.URLField(
+        unique=True,
+        default="https://github.com/2021fallCMPUT404/group-cmput404-project")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=256, unique=False)
+
 
 class Inbox(models.Model):
-	type = 'inbox'
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = 'inbox'
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -3,43 +3,56 @@ from . import views
 from django.http import HttpResponse
 from .models import User, Create_user, User_Profile
 from django.apps import apps
+from . import create_user_form
 
 Post_model = apps.get_model('Posts', 'Post')
+
+
 # Create your views here.
 def homepage(request):
-	return HttpResponse("Placeholder homepage")
+    return HttpResponse("Placeholder homepage")
+
 
 def placeholder(request, User_id):
-	#latest_user_list = User.objects.order_by('-id')[:5]
-	#output = ','.join([str(q.username) for q in latest_user_list])
-	user = get_object_or_404(User, pk=User_id)
-	output = 'User id is: {}, Username is: {}, passoword is: {}'.format(user.id, user.username, user.password)
-	return HttpResponse(output)
+    #latest_user_list = User.objects.order_by('-id')[:5]
+    #output = ','.join([str(q.username) for q in latest_user_list])
+    user = get_object_or_404(User, pk=User_id)
+    output = 'User id is: {}, Username is: {}, passoword is: {}'.format(
+        user.id, user.username, user.password)
+    return HttpResponse(output)
+
 
 def user_post_view(request, User_id):
-	user = get_object_or_404(User, pk=User_id)
-	latest_post_list = Post_model.objects.all().filter(author__id=user.id)
-	print(latest_post_list)
-	return render(request, 'Posts/placeholder.html', {'latest_post_list': latest_post_list})
+    user = get_object_or_404(User, pk=User_id)
+    latest_post_list = Post_model.objects.all().filter(author__id=user.id)
+    print(latest_post_list)
+    return render(request, 'Posts/placeholder.html',
+                  {'latest_post_list': latest_post_list})
+
 
 def index(request):
     my_dict = {'insert_me': "This line is from users/index.html"}
     return render(request, 'users/index.html', context=my_dict)
 
-def create_user_view(request):
-	form = Create_user()
-	if request.method == "POST":
-		form = Create_user(request.POST)
-		if form.is_valid():
-			print("OK")
-			#TODO: CHECK IF THE USER ALREADY EXISTS IN THE DATABASE
-			new_user = User.objects.create_user(username=form.cleaned_data['username'],  password=form.cleaned_data['password'])
-			user_prof = User_Profile(user=new_user)
-			print(user_prof.user, user_prof.type)
-			print(new_user.id)
-			user_prof.save()
-		else:
-			print("not ok")
-			
-	return render(request, 'users/create_user.html', {'form':form})
 
+def create_user_view(request):
+    #form = Create_user()
+    form = create_user_form.create_new_user(request.POST)
+    if request.method == "POST":
+        #form = Create_user(request.POST)
+        form = create_user_form.create_new_user(request.POST)
+        if form.is_valid():
+            print("OK")
+            #TODO: CHECK IF THE USER ALREADY EXISTS IN THE DATABASE
+            new_user = User.objects.create_user(
+                username=form.cleaned_data['displayName'],
+                password=form.cleaned_data['password'])
+            user_prof = User_Profile(user=new_user)
+            print(user_prof.user, user_prof.type)
+            print(new_user.id)
+            user_prof.save()
+            form.clean()
+        else:
+            print("not ok")
+
+    return render(request, 'users/create_user.html', {'form': form})

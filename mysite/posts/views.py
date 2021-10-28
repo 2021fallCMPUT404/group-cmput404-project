0c4d777cdb94f548e31a5caa68cd2eeff284ce55
FrankQixin <qd@ualberta.ca>
2021-10-28 02:41:39 -0600

    privacy update (public ,only me)

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -17,18 +17,7 @@ from django.core.exceptions import PermissionDenied
 def post(request, Post_id):
     post = get_object_or_404(Post, pk=Post_id)
     share_form = ShareForm()
-    current_user=User.objects.get(id=request.user.id)
-    authorized_posts=[]
-    #for p in post:
-    #   if
-    #        authorized_posts.append(p)
-    #    elif p.privacy==1:
-    #        if p.author==current_user:
-    #            authorized_posts.append(p)
-    #authorized_posts.sort(key="pub_date")
-
-
-
+    
     return render(request, 'posts/post.html', {'post':post})
     #output = "Post text is: {}, Post date is: {}, Post id is: {}, Post author is: {}".format(post.text, post.pub_date,post.id, post.author)
     #return HttpResponse(output)
@@ -36,11 +25,26 @@ def post(request, Post_id):
 def placeholder(request):
     latest_post_list = Post.objects.order_by('-pub_date')[:5]
     template = loader.get_template('posts/placeholder.html')
+    current_user=User.objects.get(id=request.user.id)
+    authorized_posts=[]
+    print(current_user)
+    for p in latest_post_list:
+        if   p.privacy==0:          #PUBLIC POST
+            authorized_posts.append(p)
+        elif p.privacy==1:          #PRIVATE POST
+            if p.author==current_user:
+                authorized_posts.append(p)
+
+    #NEED FRIEND POST
+
+    
     #output = '\n'.join([q.text for q in latest_post_list])
     #print(latest_post_list)
+    
     context = {
-        'latest_post_list': latest_post_list
+        'latest_post_list': authorized_posts
     }
+    
     return HttpResponse(template.render(context, request))
 
 def delete_post(request,Post_id):

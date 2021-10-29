ade5923832f1c80f5cfeb2ff71659a064138e655
Henry417E1610 <renyu@ualberta.ca>
2021-10-29 12:22:00 -0600

    reverted user test

--- mysite/users/tests.py
+++ mysite/users/tests.py
@@ -1,63 +1,6 @@
 from django.forms.fields import EmailField
 from django.test import TestCase
-<<<<<<< HEAD
 from django.contrib.auth.models import User
-from .models import User_Profile
-
-
-class UsersTestCase(TestCase):
-    def setUp(self):
-        user1 = User.objects.create(username='testcase',
-                                    first_name='test',
-                                    last_name='case',
-                                    email='testcase@ualberta.ca',
-                                    password='12345')
-        User_Profile.objects.create(
-            displayName='case_1',
-            user=user1,
-            profileImage='test_image.jpg',
-            github='https://github.com/orgs/2021fallCMPUT404/dashboard',
-            bio='test_bio1')
-
-        user2 = User.objects.create(username='testcase2',
-                                    first_name='test2',
-                                    last_name='case2',
-                                    email='testcase2@ualberta.ca',
-                                    password='123456')
-        User_Profile.objects.create(
-            displayName='case_2',
-            user=user2,
-            profileImage='test_image.jpg',
-            github='https://github.com/orgs/2021fallCMPUT404/dashboard',
-            bio='test_bio2')
-
-    def test_user_model(self):
-        """Animals that can speak are correctly identified"""
-        test_user_1 = User.objects.get(username="testcase")
-        test_profile_1 = User_Profile.objects.get(user=test_user_1)
-        test_user_2 = User.objects.get(username="testcase2")
-        test_profile_2 = User_Profile.objects.get(user=test_user_2)
-        self.assertEqual(test_profile_1.displayName, 'case_1')
-        self.assertEqual(test_profile_2.displayName, 'case_2')
-
-        self.assertEqual(test_profile_1.bio, 'test_bio1')
-        self.assertEqual(test_profile_2.bio, 'test_bio2')
-
-        self.assertEqual(test_profile_1.profileImage, 'test_image.jpg')
-        self.assertEqual(test_profile_2.profileImage, 'test_image.jpg')
-
-        self.assertEqual(test_profile_1.github,
-                         'https://github.com/orgs/2021fallCMPUT404/dashboard')
-        self.assertEqual(test_profile_2.github,
-                         'https://github.com/orgs/2021fallCMPUT404/dashboard')
-        '''
-        self.assertEqual(test_profile_1.displayName, 'case_1')
-        self.assertEqual(test_profile_2.displayName, 'case_2')
-        '''
-=======
-from django.urls import reverse
-from django.test.utils import setup_test_environment
-from django.test import Client
 from .models import Create_user, User, User_Profile, UserFollows, FriendRequest
 
 # Create your tests here.
@@ -94,4 +37,3 @@ class UserViewsTests(TestCase):
         response = self.client.get(reverse('users:homepage'))
         self.assertTrue(response.status_code != 404)
 
->>>>>>> f03a138ddd73cc0a366506103e575fb55a2e23a5

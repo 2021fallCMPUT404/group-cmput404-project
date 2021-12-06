4cff05e5d6d423475472aa6b0298eab66c205250
Henry417E1610 <renyu@ualberta.ca>
2021-12-06 13:02:07 -0700

    made minor updates and fixed json decoding issues

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -944,8 +944,8 @@ def get_t15_posts(url):
                                auth=('connectionsuperuser', '404connection'),
                                headers={'Referer': "http://127.0.0.1:8000/"})
 
-    ext_request = ext_request.json()
-    return ext_request
+    req = ext_request.json()
+    return req
 
 
 def view_t15_posts(request):
